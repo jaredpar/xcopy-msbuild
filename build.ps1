a0d6993bf76c66e5adbb88251ab19324800517f5
Jared Parsons <jaredpar@microsoft.com>
2017-02-12 17:38:17 -0800

    Compose together a simple MSBuild output

--- build.ps1
+++ build.ps1
@@ -3,12 +3,15 @@ set-strictmode -version 2.0
 $ErrorActionPreference="Stop"
 
 $repoDir = $PSScriptRoot
+$binariesDir = join-path $repoDir "binaries"
+$msbuildDir = join-path  $binariesDir "msbuild"
 
-# TODO: fix this
-$packagesDir = "e:\nuget"
+# TODO: fix this to have the standard calculated values
+$packagesDir = "e:\temp\msbuild"
 
+# Download all of the packages needed to compose the xcopy MSBuild
 function download-packages() {
-    $nuget = join-path $repoDir "binaries\tools\nuget.exe"
+    $nuget = join-path $binariesDir "tools\nuget.exe"
 
     write-host "Downloadnig nuget.exe"
     & src\download-nuget.ps1 -destPath $nuget -version "3.6.0-beta1"
@@ -17,10 +20,40 @@ function download-packages() {
     & src\restore.ps1 -nuget $nuget -packagesDir $packagesDir
 }
 
+function get-packagemap() {
+    $map = @{}
+    foreach ($line in gc (join-path $repoDir "src\packages.txt")) {
+        $all = $line.split(':');
+        $map[$all[0]] = $all[1]
+    }
+    return $map
+}
+
+function compose-packages() { 
+    write-host "Composing packages"
+    mkdir $msbuildDir -ErrorAction SilentlyContinue | out-null
+    $map = get-packagemap
+    foreach ($k in $map.Keys) {
+        $d = join-path $packagesDir "$($k).$($map[$k])"
+        switch -wildcard ($k) {
+            "Microsoft.Build.Runtime" { 
+                cp -re -fo (join-path $d "contentFiles\any\net46\*") $msbuildDir
+                break
+            }
+            "Microsoft.Build*" { 
+                cp -re -fo (join-path $d "lib\net46\*") $msbuildDir
+                break
+            }
+            default { throw "Did not account for $k" }
+        }
+    }
+}
+
 try {
     pushd $repoDir
     
     download-packages
+    compose-packages
 
     exit 0
 }
