a9c1be443e149da03e44bf5eab86e772f94b8478
Jared Parsons <jaredpar@microsoft.com>
2017-02-12 18:59:40 -0800

    NuGet assets partially composed

--- build.ps1
+++ build.ps1
@@ -9,6 +9,13 @@ $msbuildDir = join-path  $binariesDir "msbuild"
 # TODO: fix this to have the standard calculated values
 $packagesDir = "e:\temp\msbuild"
 
+# TODO: hacky values that work for now
+$msbuildVersion = "15.0"
+
+$importPropsBeforeDir = join-path (join-path $msbuildDir $msbuildVersion) "Imports\Microsoft.Common.props\ImportBefore"
+$importTargetsBeforeDir = join-path (join-path $msbuildDir $msbuildVersion) "Imports\Microsoft.Common.targets\ImportBefore"
+$importTargetsAfterDir = join-path (join-path $msbuildDir $msbuildVersion) "Imports\Microsoft.Common.targets\ImportAfter"
+
 # Download all of the packages needed to compose the xcopy MSBuild
 function download-packages() {
     $nuget = join-path $binariesDir "tools\nuget.exe"
@@ -29,10 +36,15 @@ function get-packagemap() {
     return $map
 }
 
+# Compose all of the MSBuild packages together into a valid MSBuild layout
 function compose-packages() { 
-    write-host "Composing packages"
+    write-host "Composing package components"
+
     mkdir $msbuildDir -ErrorAction SilentlyContinue | out-null
-    rm -re "$msbuildDir\*"
+    rm -re -fo "$msbuildDir\*"
+    mkdir $importPropsBeforeDir | out-null
+    mkdir $importTargetsBeforeDir | out-null
+    mkdir $importTargetsAfterDir | out-null
 
     $map = get-packagemap
     foreach ($k in $map.Keys) {
@@ -43,17 +55,17 @@ function compose-packages() {
                 break
             }
             "Microsoft.Build*" { 
-                cp -re -fo (join-path $d "lib\net46\*") $msbuildDir
+                cp -re (join-path $d "lib\net46\*") $msbuildDir
                 break
             }
             "Microsoft.Net.Compilers" {
                 $roslynDir = join-path $msbuildDir "Roslyn"
-                mkdir $roslynDir | out-null
-                cp -re -fo (join-path $d "tools\*") $roslynDir
+                mkdir $roslynDir -ErrorAction SilentlyContinue | out-null
+                cp -re (join-path $d "tools\*") $roslynDir
                 break
             }
             "System.Collections.Immutable" {
-                cp -re -fo (join-path $d "lib\netstandard1.0\*") $msbuildDir
+                cp -re (join-path $d "lib\netstandard1.0\*") $msbuildDir
                 break
             }
             default { throw "Did not account for $k" }
@@ -66,11 +78,41 @@ function run-tests() {
     & src\run-tests.ps1 $msbuild
 }
 
+function compose-others() {
+    compose-project-json
+    compose-portable
+}
+
+# TODO: remove this step once we have a valid package source
+#
+# The project.json components aren't presently available as a Nuget package.  Compose them 
+# together here from an existing MSBuild installation.
+function compose-project-json() { 
+    write-host "Composing other components"
+    $sourceDir = "C:\Program Files (x86)\msbuild\Microsoft\NuGet"
+    $destDir = join-path $msbuildDir "Microsoft\NuGet"
+    mkdir $destDir | out-null
+    cp -re "$sourceDir\*" $destDir
+    cp "C:\Program Files (x86)\msbuild\14.0\Imports\Microsoft.Common.Props\ImportBefore\Microsoft.NuGet.ImportBefore.props" $importPropsBeforeDir
+    cp "C:\Program Files (x86)\MSBuild\14.0\Microsoft.Common.Targets\ImportAfter\Microsoft.NuGet.ImportAfter.targets" $importTargetsAfterDir
+}
+
+# TODO: remove this step once we have a valid portable location
+function compose-portable() {
+    $portableDir = join-path $msbuildDir "Microsoft\Portable"
+    mkdir $portableDir | out-null
+    $sourceDir = "C:\Program Files (x86)\MSBuild\Microsoft\Portable"
+
+    cp (join-path $sourceDir "Microsoft.Portable.*") $portableDir
+    cp -re (join-path $sourceDir "v5.0") $portableDir
+}
+
 try {
     pushd $repoDir
     
     download-packages
     compose-packages
+    compose-others
 
     if ($test) {
         run-tests
